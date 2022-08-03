c4a82336cd1cd33f57710fad21a8fc007fd8b3c3
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2022-08-03 23:30:16 +0300

    Add files via upload

---
+++ BarakaSelfKYC/Sources/BarakaSelfKYC.xcframework/ios-arm64/BarakaSelfKYC.framework/Headers/BarakaSelfKYC.h
@@ -0,0 +1,18 @@
+//
+//  BarakaSelfKYC.h
+//  BarakaSelfKYC
+//
+//  Created by Muhsin Etki on 3.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BarakaSelfKYC.
+FOUNDATION_EXPORT double BarakaSelfKYCVersionNumber;
+
+//! Project version string for BarakaSelfKYC.
+FOUNDATION_EXPORT const unsigned char BarakaSelfKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarakaSelfKYC/PublicHeader.h>
+
+
---
+++ BarakaSelfKYC/Tests/BarakaSelfKYCTests/BarakaSelfKYCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BarakaSelfKYC
+
+final class BarakaSelfKYCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BarakaSelfKYC().text, "Hello, World!")
+    }
+}
